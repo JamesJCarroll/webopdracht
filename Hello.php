b44e5cba40c3c539f02655141a5e2c6df7890701
James Carroll <j.j.carroll@students..uu.nl>
2018-12-10 22:01:33 +0100

    .

--- Hello.php
+++ Hello.php
@@ -28,9 +28,9 @@ mail ($mail, $subject , $massage, $headers);
 		</menu>
 
 		
-	  <p> beste <?php echo $naam ?>
+	  <p> beste <?php echo $naam; ?>
 	  <br><br>
-	 	We sturen een email naar: <?php echo $email ?> . <br>
+	 	We sturen een email naar: <?php echo $email; ?> . <br>
 	  	We hopen dat je onze mail ontvangt! <br>
 	   <br><br>
 		Groetjes, team 31
