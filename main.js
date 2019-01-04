fee8b74706f721ee98ed5c47b8fe05a102c7c2e5
James Carroll <j.j.carroll@students..uu.nl>
2019-01-04 19:29:53 +0100

    .

--- main.js
+++ main.js
@@ -37,6 +37,15 @@
 				.appendTo($body);
 
 		// Panel.
+
+		// When the user clicks on <div>, open the popup
+		<script>
+		function myFunction() {
+  		var popup = document.getElementById("myPopup");
+  		popup.classList.toggle("show");
+		}
+		</script>
+
 			$(
 				'<div id="navPanel">' +
 					'<nav>' +
